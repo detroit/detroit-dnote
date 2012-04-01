63255f3a1ff79ec5742c035016c17213e7aa124d
7rans <transfire@gmail.com>
2012-04-01 12:24:12 -0400

    Add NOTICE.rdoc file. [admin]

--- lib/detroit-dnote.rb
+++ lib/detroit-dnote.rb
@@ -1,21 +1,6 @@
-# Copyright (c) 2011 Rubyworks 
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-require 'detroit/tool'
-
 module Detroit
+
+  require 'detroit/tool'
 
   # The Developmer's Notes tool goes through source files
   # and compiles a list of any labeled comments. Labels are
@@ -213,3 +198,18 @@ module Detroit
   end
 
 end
+
+# Copyright (c) 2011 Rubyworks 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
