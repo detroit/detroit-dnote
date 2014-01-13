82e7ad89ccee9d2c56296fcfc893bf15979d55c4
7rans <transfire@gmail.com>
2014-01-13 18:22:10 -0500

    Update to work with Detroit 0.4.

--- lib/detroit-dnote.rb
+++ lib/detroit-dnote.rb
@@ -1,8 +1,8 @@
+require 'detroit-standard'
+
 module Detroit
-
-  require 'detroit/tool'
-
-  # The Developmer's Notes tool goes through source files
+  ##
+  # The Developer Notes tool goes through source files
   # and compiles a list of any labeled comments. Labels are
   # all-caps single word prefixes to a comment ending in a
   # colon and space.
@@ -11,22 +11,34 @@ module Detroit
   #
   class DNote < Tool
 
-    # not that this is not necessary, but ...
-    # def self.available?(project)
-    #  begin
-    #    require 'dnote'
-    #    require 'dnote/format'
-    #    true
-    #  rescue LoadError
-    #    false
-    #  end
-    #end
-
+    # Works with the Standard assembly.
     #
-    DEFAULT_FILES = "**/*.rb"
+    # @!parse
+    #   include Standard
+    #
+    assembly Standard
+
+    # Location of manpage for this tool.
+    MANPAGE = File.dirname(__FILE__) + '/../man/detroit-dnote.5'
+
+    # Default glob of files to look in for notes. Presently defaults
+    # all Ruby and C scripts (i.e. `*.rb` and `*.c` files).
+    DEFAULT_FILES = "**/*.{rb,c}"
 
     # Default note labels to looked for in source code.
     DEFAULT_LABELS = ['TODO', 'FIXME', 'OPTIMIZE', 'DEPRECATE']
+
+    # Load requirements and set attribute defaults.
+    #
+    # @return [void]
+    def prerequisite
+      require 'dnote'
+      require 'dnote/format'
+
+      @files   = DEFAULT_FILES
+      @output  = project.log + 'dnotes.html'
+      @labels  = nil #DEFAULT_LABELS
+    end
 
     # Specific labels to document.
     attr_accessor :labels
@@ -49,20 +61,21 @@ module Detroit
     # Output is either a file name with a clear extension to infer type
     # or a list of such file names, or a hash mapping file name to type.
     #
-    #   output: NOTES.rdoc
+    # @example
+    #   output: NOTES.md
     #
+    # @example
     #   output:
-    #     - NOTES.rdoc
+    #     - NOTES.md
     #     - site/notes.html
     #
+    # @example
     #   output:
     #     NOTES: markdown
     #     site/notes.html: html
     #
-    # Recognized formats include `xml`, `html` and `rdoc` among others.
+    # Recognized formats include `xml`, `html`, `md` and `rdoc` among others.
     attr_accessor :output
-
-    #  S E R V I C E  M E T H O D S
 
     # Check the output file and see if they are older than
     # the input files.
@@ -76,13 +89,16 @@ module Detroit
     end
 
     # Generate notes documents.
+    #
+    # @return [void]
     def document
       session = dnote_session
 
       output_mapping.each do |file, format|
         #next unless verify_format(format)
 
-        mkdir_p(File.dirname(file))
+        dir = File.dirname(file)
+        mkdir_p(dir) unless File.directory?(dir)
 
         session.output = file
         session.format = format
@@ -93,6 +109,8 @@ module Detroit
     end
 
     # Reset output files, marking them as out-of-date.
+    #
+    # @return [void]
     def reset
       output.each do |file, format|
         if File.exist?(file)
@@ -103,6 +121,8 @@ module Detroit
     end
 
     # Remove output files.
+    #
+    # @return [void]
     def purge
       output.each do |file, format|
         if File.exist?(file)
@@ -112,33 +132,21 @@ module Detroit
       end
     end
 
-    # A S S E M B L Y  M E T H O D S
-
     #
     def assemble?(station, options={})
-      case station
-      when :document then true
-      when :reset    then true
-      when :purge    then true
-      end
-    end
-
-    #
-    def assemble(station, options={})
-      case station
-      when :document then document
-      when :reset    then reset
-      when :purge    then purge
-      end
+      return true if station == :document
+      return true if station == :reset
+      return true if station == :purge
+      return false
     end
 
   private
 
-    # TODO: apply_naming_policy ?
-
-    #
     # Convert output into a hash of `file => format`.
     #
+    # @todo Should we use #apply_naming_policy ?
+    #
+    # @return [Hash]
     def output_mapping
       @output_mapping ||= (
         hash = {}
@@ -156,14 +164,20 @@ module Detroit
       )
     end
 
+    # The format of the file based on the extension.
+    # If the file has no extension then the value of 
+    # `DEFAULT_FORMAT` is returned.
     #
+    # @return [String]
     def format(file)
       type = File.extname(file).sub('.','')
       type = DEFAULT_FORMAT if type.empty?
       type
     end
 
+    # DNote Session instance.
     #
+    # @return [DNote::Session]
     def dnote_session
       ::DNote::Session.new do |s|
         s.paths   = files
@@ -174,25 +188,6 @@ module Detroit
         s.context = lines
         s.dryrun  = trial?
       end
-    end
-
-    #
-    def initialize_requires
-      require 'dnote'
-      require 'dnote/format'
-    end
-
-    #
-    def initialize_defaults
-      @files   = DEFAULT_FILES
-      @output  = project.log + 'dnotes.html'
-      @labels  = nil #DEFAULT_LABELS
-    end
-
-  public
-
-    def self.man_page
-      File.dirname(__FILE__)+'/../man/detroit-dnote.5'
     end
 
   end
